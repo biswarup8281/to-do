4f33a397c9098dee56131de1f788aa7249eb5e05
biswarup8281 <biswarup8281@yahoo.com>
2020-02-15 18:16:54 +0530

    added lineThrough and checkbox

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Todo } from '../../models/Todos';
+import { TodoService } from "../../services/todo.service";
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+todo: Todo[];
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
+
+    this.todo = this.todoService.getTodos();
+
   }
 
 }
